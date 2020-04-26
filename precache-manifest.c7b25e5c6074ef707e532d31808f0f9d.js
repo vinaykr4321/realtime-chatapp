06ffb157657c2bce03e4845418c48ecd65388218
vinay <vinaykr4321@gmail.com>
2020-04-26 16:40:54 +0530

    Updates

---
+++ precache-manifest.c7b25e5c6074ef707e532d31808f0f9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6673b0314d9bb7fcd5fef5a31f072e3",
+    "url": "/realtime-chatapp/index.html"
+  },
+  {
+    "revision": "9096d626d7d2bd93c4ac",
+    "url": "/realtime-chatapp/static/css/main.1ce55ba0.chunk.css"
+  },
+  {
+    "revision": "ef4af58dc31e413abb80",
+    "url": "/realtime-chatapp/static/js/2.105a8349.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/realtime-chatapp/static/js/2.105a8349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9096d626d7d2bd93c4ac",
+    "url": "/realtime-chatapp/static/js/main.780458c4.chunk.js"
+  },
+  {
+    "revision": "66d2b90b53f77e08d787",
+    "url": "/realtime-chatapp/static/js/runtime-main.83c85a33.js"
+  }
+]);
